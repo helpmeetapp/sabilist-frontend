d415a7a171947e0f5e0ce8c6af625057e19576d4
manuelExtra <olaleyeemmanuel23@gmail.com>
2024-03-16 16:21:58 +0100

    landing page footer adjustments

--- src/components/footer/Index.tsx
+++ src/components/footer/Index.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="h-full w-xcreen bg-[#2D2D2D] text-[#F8F9FF]">
-      <div className="max-w-7xl mx-auto pt-4 md:pt-16 px-4 flex flex-col md:flex-row">
+      <div className="max-w-7xl mx-auto pt-10 md:pt-16 px-4 flex flex-col md:flex-row">
         <div className="md:w-[30%]">
           <h5 className="text-2xl">Sabilist</h5>
         </div>
 
-        <div className="md:w-[70%] w-full grid grid-cols-2 lg:grid-cols-4 text-xl">
+        <div className="md:w-[70%] w-full grid grid-cols-2 lg:grid-cols-4 text-xl mt-10 md:mt-0">
           <div className="space-y-2 mb-10">
             <h5 className="text-xl font-bold">Product</h5>
             <ul className="text-base font-normal space-y-2">
